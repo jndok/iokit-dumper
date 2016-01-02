cb0bec2600447636cd0b660144a7a01a2dcc8ea8
jndok <me@jndok.net>
2016-01-02 12:33:36 +0100

    minor fixes

--- dumper/dumper.c
+++ dumper/dumper.c
@@ -17,21 +17,21 @@ char *read_line(FILE *fin) {
     int read_chars = 0;
     int bufsize = INITIAL_ALLOC;
     char *line = malloc(bufsize);
-    
+
     if ( !line ) {
         return NULL;
     }
-    
+
     buffer = line;
-    
+
     while ( fgets(buffer, bufsize - read_chars, fin) ) {
         read_chars = (int)strlen(line);
-        
+
         if ( line[read_chars - 1] == '\n' ) {
             line[read_chars - 1] = '\0';
             return line;
         }
-        
+
         else {
             bufsize = 2 * bufsize;
             tmp = realloc(line, bufsize);
@@ -52,97 +52,97 @@ uint32_t dump_hierarchy(task_t kport, uint64_t read_addr, const char *path, bool
 {
     uint64_t TEXT_SEG_START_ADDR=0, TEXT_SEG_END_ADDR=0, TEXT_SEG_SIZE=0;
     uint64_t CONST_SECT_START_ADDR=0, CONST_SECT_SIZE=0, COMMON_SECT_START_ADDR=0, COMMON_SECT_END_ADDR=0, COMMON_SECT_SIZE=0;
-    
+
     struct mach_header_64 *header = find_mach_header_kmem_addr(kport, SLIDE_POINTER(read_addr));
-    
+
     void *load_commands = read_kernel_memory(kport, SLIDE_POINTER(read_addr+sizeof(struct mach_header_64)), header->sizeofcmds);
-    
+
     struct segment_command_64 *TEXT_SEG = find_segment_command_kmem(header, load_commands, SEG_TEXT);
     struct segment_command_64 *DATA_SEG = find_segment_command_kmem(header, load_commands, SEG_DATA);
-    
+
     if (!TEXT_SEG || !DATA_SEG) {
         __dbg("(!) Invalid KEXT.");
         return 2;
     }
-    
+
     struct section_64 *DATA_CONST_SECT = find_section_command_kmem(DATA_SEG, SECT_CONST);
     struct section_64 *DATA_COMMON_SECT = find_section_command_kmem(DATA_SEG, SECT_COMMON);
-    
+
     if (!DATA_CONST_SECT || !DATA_COMMON_SECT) {
         __dbg("(!) Invalid KEXT.");
         return 2;
     }
-    
+
     TEXT_SEG_START_ADDR =       TEXT_SEG->vmaddr;
     TEXT_SEG_SIZE =             TEXT_SEG->vmsize;
     TEXT_SEG_END_ADDR =         TEXT_SEG_START_ADDR + TEXT_SEG_SIZE;
-    
+
     CONST_SECT_START_ADDR =     DATA_CONST_SECT->addr;
     CONST_SECT_SIZE =           DATA_CONST_SECT->size;
-    
+
     COMMON_SECT_START_ADDR =    DATA_COMMON_SECT->addr;
     COMMON_SECT_SIZE =          DATA_COMMON_SECT->size;
     COMMON_SECT_END_ADDR =      COMMON_SECT_START_ADDR + COMMON_SECT_SIZE;
-    
+
     void *const_sect_buffer = malloc(CONST_SECT_SIZE);
     for (uint32_t i=0; i<CONST_SECT_SIZE; i+=128) {
         read_kernel_memory_in_buffer(kport, CONST_SECT_START_ADDR+i, 128, const_sect_buffer+i);
     }
-    
+
     uint64_t vtable_ptrs[16];
     uint32_t vtable_ptrs_cnt=0;
-    
+
     if (overwrite) {
         INIT_OUTPUT_FILE(path);
     }
-    
+
     for (uint32_t i=0; i<CONST_SECT_SIZE; i+=8) {
         if (vtable_ptrs_cnt==13) {  //we are in a vtable
             if (IS_VALID_POINTER(vtable_ptrs[7], TEXT_SEG_START_ADDR, TEXT_SEG_END_ADDR)) {
-                
+
                 struct hierarchy_entry *k, *last=NULL, *last_parent=NULL, *last_entry=NULL;
-                
+
                 int found_in_file=0;
-                
+
                 FILE *fin;
                 char *line;
-                
+
                 uint64_t gMetaClass_addr = calculate_gMetaClass_addr_from_getMetaClass(kport, vtable_ptrs[7]);
                 if (!IS_VALID_POINTER(gMetaClass_addr, COMMON_SECT_START_ADDR, COMMON_SECT_END_ADDR)) {
                     goto end;
                 }
-                
+
                 SLIST_HEAD(hierarchy_head, hierarchy_entry) hierarchy_h = SLIST_HEAD_INITIALIZER(hierarchy_h);
                 SLIST_HEAD(hierarchy_write_head, hierarchy_entry) sub_hierarchy_h = SLIST_HEAD_INITIALIZER(sub_hierarchy_h);
-                
+
                 SLIST_INIT(&hierarchy_h);
                 SLIST_INIT(&sub_hierarchy_h);
-                
+
                 while (1) {
                     MetaClass_Object_t *meta = parse_MetaClass_object(kport, gMetaClass_addr);
                     OSSymbol_Object_t *symbol = parse_OSSymbol_object(kport, meta->OSSymbol_ptr);
-                    
+
                     struct hierarchy_entry *entry = malloc(sizeof(struct hierarchy_entry));
                     entry->name=getOSSymbolName(kport, symbol);
                     SLIST_INSERT_HEAD(&hierarchy_h, entry, entries);
-                    
+
                     if (meta->Parent_gMetaClass_ptr==0) {
                         break;
                     }
-                    
+
                     gMetaClass_addr=meta->Parent_gMetaClass_ptr;
                 }
-                
+
                 SLIST_FOREACH(k, &hierarchy_h, entries)
                 last=k;
-                
+
                 char search[128];
-                
+
                 SLIST_FOREACH(k, &hierarchy_h, entries) {
-                    
+
                     strcpy(search, k->name);
                     strcat(search, " ");        //this is needed for some rare cases
-                    
+
                     fin=fopen(path, "r");
                     while ((line = read_line(fin)) ) {
                         if (strstr(line, search)) {
@@ -151,11 +151,11 @@ uint32_t dump_hierarchy(task_t kport, uint64_t read_addr, const char *path, bool
                         } else {
                             found_in_file=0;
                         }
-                        
+
                         free(line);
                     }
                     fclose(fin);
-                    
+
                     if (found_in_file) {
                         last_parent=k;
                     } else {
@@ -171,12 +171,12 @@ uint32_t dump_hierarchy(task_t kport, uint64_t read_addr, const char *path, bool
                             }
                         }
                     }
-                    
+
                     bzero(search, 128);
                 }
-                
+
                 fin=fopen(path, "a");
-                
+
                 if (!last_parent) {
                     struct hierarchy_entry *entry;
                     SLIST_FOREACH(entry, &hierarchy_h, entries) {
@@ -189,7 +189,7 @@ uint32_t dump_hierarchy(task_t kport, uint64_t read_addr, const char *path, bool
                     }
                 } else if (last_parent && !found_in_file) {
                     fprintf(fin, "%s -> ", last_parent->name);
-                    
+
                     struct hierarchy_entry *entry;
                     SLIST_FOREACH(entry, &sub_hierarchy_h, entries) {
                         fprintf(fin, "%s ", entry->name);
@@ -200,11 +200,11 @@ uint32_t dump_hierarchy(task_t kport, uint64_t read_addr, const char *path, bool
                         }
                     }
                 }
-                
+
                 fclose(fin);
-                
+
             end:;
-                
+
             }
             vtable_ptrs_cnt=0;
             for ( ;; i+=8) {    //advance to next vtable
@@ -214,8 +214,8 @@ uint32_t dump_hierarchy(task_t kport, uint64_t read_addr, const char *path, bool
                 }
             }
         }
-        
-        
+
+
         uint64_t curr_ptr = *(uint64_t*)(const_sect_buffer+i);
         if (curr_ptr!=0x0) {
             vtable_ptrs[vtable_ptrs_cnt]=curr_ptr;
@@ -227,10 +227,11 @@ uint32_t dump_hierarchy(task_t kport, uint64_t read_addr, const char *path, bool
             return 2;
         }
     }
-    
+
     if (overwrite) {
         CLOSE_OUTPUT_FILE(path);
     }
-        
+
+    __dbg("(+) Done! Written hierarchy for %#llx @ path: %s", read_addr, path);
     return 0;
 }
--- main.c
+++ main.c
@@ -2,9 +2,9 @@
  *  iokit-dumper –– v1
  *
  *  This tool is useful for dumping the IOKit class hierarchy of the kernel
- *  or any KEXT. It generates a DOT file that can be processed with the dot engine 
- *  to create a visual graph. 
- *  
+ *  or any KEXT. It generates a DOT file that can be processed with the dot engine
+ *  to create a visual graph.
+ *
  *  I am aware that this can be done much more cleanly with scripts
  *  for IDA, but since I don't have IDA and don't know how to script for it,
  *  I decided to do it myself :p
@@ -31,7 +31,6 @@
 
 #include <stdio.h>
 #include <fcntl.h>
-#include <sys/queue.h>
 
 #include "import.h"
 
@@ -39,7 +38,7 @@ extern uint64_t kslide;
 extern uint64_t KextUnslidBaseAddress(const char *KextBundleName);
 
 int main(int argc, const char * argv[]) {
-    
+
     if (argc < 3) {
         fprintf(stderr, "Usage: ./exec kernel/[kext_bundle_id] [output_file] [overwrite]\n");
         fprintf(stderr, "\tkernel/[kext_bundle_id]: Pass the \'kernel\' string without quotes to dump the kernel hierarchy. Pass a KEXT bundle ID to dump that KEXT hierarchy.\n");
@@ -47,11 +46,11 @@ int main(int argc, const char * argv[]) {
         fprintf(stderr, "\t[overwrite]: Boolean. Pass 0 or 1. If the hierarchy in DOT format should be appended to [output_file] or if the content should be overwritten.\n");
         return 1;
     }
-    
+
     uint64_t READ_BASE=0;
     const char *OUT_FILE=argv[2];
     boolean_t overwrite=TRUE;
-    
+
     if (strcmp(argv[1], "kernel") == 0) {
         READ_BASE=0xffffff8000200000;
     } else {
@@ -61,7 +60,7 @@ int main(int argc, const char * argv[]) {
             return 1;
         }
     }
-    
+
     if (open(argv[2], O_RDONLY) < 0) {
         __dbg("(!) File was not found on disk. Creating it now.");
         if(open(argv[2], O_CREAT|O_RDWR, 0666) < 0) {
@@ -69,7 +68,7 @@ int main(int argc, const char * argv[]) {
             return 1;
         }
     }
-    
+
     if (argv[3] != NULL) {
         if (atoi(argv[3]) == FALSE) {
             overwrite=FALSE;
@@ -77,15 +76,12 @@ int main(int argc, const char * argv[]) {
             overwrite=TRUE;
         }
     }
-    
+
     SET_KERNEL_SLIDE(kslide);
     mach_port_t kport = get_kernel_task();
     if (!kport) {
         return -2;
     }
-    
-    dump_hierarchy(kport, READ_BASE, OUT_FILE, overwrite);
-     
-    return 0;
-}
 
+    return dump_hierarchy(kport, READ_BASE, OUT_FILE, overwrite);;
+}
